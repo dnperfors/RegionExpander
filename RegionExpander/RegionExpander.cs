4692164f0406f15e98d9828359e28c89d44d8d84
David Perfors <dnperfors@gmail.com>
2017-03-24 21:40:19 +0100

    Add first version of RegionExpander

---
+++ RegionExpander/RegionExpander.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+
+namespace RegionExpander
+{
+    internal sealed class RegionExpander
+    {
+        private readonly IWpfTextView _view;
+        private readonly IOutliningManager _outliningManager;
+
+        public RegionExpander(IWpfTextView view, IOutliningManager outliningManager)
+        {
+            _view = view ?? throw new ArgumentNullException("view");
+            _outliningManager = outliningManager ?? throw new ArgumentNullException("outliningManager");
+            _outliningManager.RegionsCollapsed += OnRegionCollapsed;
+        }
+
+        private void OnRegionCollapsed(object sender, RegionsCollapsedEventArgs e)
+        {
+            foreach (var region in e.CollapsedRegions.Where(x => x.IsCollapsed))
+            {
+                var regionSnapshot = region.Extent.TextBuffer.CurrentSnapshot;
+                var regionText = region.Extent.GetText(regionSnapshot);
+                if (regionText.StartsWith("#region"))
+                {
+                    _outliningManager.Expand(region);
+                }
+            }
+        }
+    }
+}
---
+++ RegionExpander/RegionExpanderTextViewCreationListener.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Outlining;
+using Microsoft.VisualStudio.Utilities;
+
+namespace RegionExpander
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("CSharp")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class RegionExpanderTextViewCreationListener : IWpfTextViewCreationListener
+    {
+        [Import(typeof(IOutliningManagerService))]
+        private IOutliningManagerService _outliningManagerService;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            new RegionExpander(textView, _outliningManagerService.GetOutliningManager(textView));
+        }
+    }
+}
